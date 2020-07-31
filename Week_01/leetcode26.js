c49c86bbf02c015bf4a27ee428800742ecad4b51
cangyue1 <939877376@qq.com>
2020-07-31 16:30:45 +0800

    21 and 26

---
+++ Week_01/leetcode26.js
@@ -0,0 +1,35 @@
+// leetcode 26 删除排序数组中的重复项
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// 优解
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for(let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            nums[i+1] = nums[j]
+            i++
+        }
+    }
+    return i + 1
+}
+
+// 个人第一思路
+var removeDuplicates = function(nums) {
+   let tem = 0;
+   for(let i = 0; i < nums.length; i++) {
+       if (i === 0) {
+            if (nums[i] === nums[i+1]) tem = i;   
+       } else {
+           if (nums[i] === nums[i+1] && nums[i] !== nums[i-1]) tem = i;
+       }
+       if(nums[i] !== nums[i+1] && nums[i] === nums[i-1]) {
+           nums.splice(tem+1, i-tem);
+           i = tem;
+       } 
+   }
+   return nums.length;
+}
