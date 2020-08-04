4583c0d9ea9c24753bb329e41662a2884f10f43d
cangyue1 <939877376@qq.com>
2020-08-04 19:50:50 +0800

    leetcode144

---
+++ Week_02/leetcode144.js
@@ -0,0 +1,46 @@
+//leetcode 144 二叉树的前序遍历
+
+/**
+ * Definition for a binary tree node.
+ * */
+ function TreeNode(val) {
+      this.val = val;
+      this.left = this.right = null;
+  }
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ * 迭代法
+ */
+const preorderTraversal1 = function(root) {
+    let stack = [];
+    let output = [];
+    if (root == null) return output;
+    stack.push(root);
+    while (stack.length > 0) {
+        const node = stack.pop();
+        output.push(node.val);
+        if (node.right != null) stack.push(node.right);
+        if (node.left != null) stack.push(node.left);
+    }
+    return output;
+}
+
+// 代码极简的递归法
+const preorderTraversal2= function(root) {
+    return root ? [root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)] : []
+};
+
+// 递归法
+const preorderTraversal3 = function(root) {
+    let array = [];
+    helper(root, array);
+    return array;
+};
+const helper = function(root, array) {
+    if (root == null) return null;
+    array.push(root.val);
+    helper(root.left, array);
+    helper(root.right, array);
+}
