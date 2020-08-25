c0b2a41b3facd43eeb0afb6bd670868c0815ed02
cangyue1 <939877376@qq.com>
2020-08-25 11:24:24 +0800

    week4

--- Week_04/README.md
+++ Week_04/README.md
@@ -5,5 +5,23 @@
 证明上，在思想上证明可以使用贪心的思想得到最优解，在代码上是非常简洁的。二分查找也是很常用的思想，思想上很简洁，但是在代码实现
 上却很容易出错，因此重点和DFS以及BFS一样，主要是熟悉实现代码，要对代码模板非常熟悉。还是多刷题。
 2、使用二分查找，寻找一个半有序数组 [4, 5, 6, 7, 0, 1, 2] 中间无序的地方
-    二分查找，查找非有序的半边
+    二分查找，查找非有序的半边，测试代码如下(本目录下的leetcode33中):
+    
+    二分法查找半有序数组无序的地方
+    const look = function (nums) {
+        if (!nums) return -1;
+        let left = 0;
+        let right = nums.length - 1;
+        while(left < right - 1) {
+            let mid = left + parseInt((right - left) / 2);
+            if(nums[mid] > nums[left]) left = mid;
+            else right = mid;
+        }
+        if (nums[right] > nums[left]) return { index: left, val: nums[left] };
+        else return { index: right, val: nums[right]};
+        // return Math.min(nums[right], nums[left])
+    }
+    
+    const nums = [4, 5, 6, 7, 0, 1, 2];
+    console.log('this is return', look(nums))
  
--- Week_04/leetcode33.js
+++ Week_04/leetcode33.js
@@ -7,14 +7,14 @@
  * 二分法
  */
 const search = function(nums, target) {
-    if (!nums) return -1
+    if (!nums) return -1;
     let left = 0;
     let right = nums.length - 1;
     while(left <= right) {
         let mid = left + parseInt((right - left) / 2);
-        if (nums[left] === target) return left
-        if (nums[right] === target) return right
-        if (nums[mid] === target) return mid
+        if (nums[left] === target) return left;
+        if (nums[right] === target) return right;
+        if (nums[mid] === target) return mid;
         if (nums[mid] > nums[left]) {
             if (nums[mid] > target && nums[left] < target) right = mid - 1;
             else left = mid + 1;
@@ -26,3 +26,21 @@ const search = function(nums, target) {
     }
     return nums[right] === target?right:-1
 };
+
+// 二分法查找半有序数组无序的地方
+const look = function (nums) {
+    if (!nums) return -1;
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right - 1) {
+        let mid = left + parseInt((right - left) / 2);
+        if(nums[mid] > nums[left]) left = mid;
+        else right = mid;
+    }
+    if (nums[right] > nums[left]) return { index: left, val: nums[left] };
+    else return { index: right, val: nums[right]};
+    // return Math.min(nums[right], nums[left])
+}
+
+const nums = [4, 5, 6, 7, 0, 1, 2];
+console.log('this is return', look(nums))
