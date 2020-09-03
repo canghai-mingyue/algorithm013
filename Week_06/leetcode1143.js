4939b090784b48bf00ed38bd5f76c3ef968308d9
cangyue1 <939877376@qq.com>
2020-09-03 23:44:40 +0800

    最长公共子序列

---
+++ Week_06/leetcode1143.js
@@ -0,0 +1,28 @@
+// leetcode 1143 最长公共子序列
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = function(text1, text2) {
+    const array1 = text1.split('');
+    const array2 = text2.split('');
+    // 二维数组多一行一列，方便处理边界条件
+    const length1 = array1.length + 1;
+    const length2 = array2.length + 1;
+    const res = new Array(length1).fill(0);
+    //这种写法，fill方法应该是赋值操作，导致每一行都是同一个引用，出现错误。
+    // const result = new Array(length2).fill([...res]);
+    let result = new Array(length2).fill(0);
+    result = result.map(() => [...res]);
+    //开始遍历
+    for(let i = 1; i < length2; i++) {
+        for(let j = 1; j < length1; j++) {
+            // DP方程
+            if(array2[i-1] === array1[j-1]) result[i][j] = result[i-1][j-1] + 1;
+            else result[i][j] = Math.max(result[i-1][j], result[i][j-1]);
+        }
+    }
+    return result[length2-1][length1-1]
+};
